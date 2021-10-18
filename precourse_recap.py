56dcfadffcf97e1cb51e6b00de5ffdac20723042
rogertmalg <roger.malgueira@outlook.com>
2021-10-18 17:00:43 +0100

    First commit

---
+++ precourse_recap.py
@@ -0,0 +1,12 @@
+number = 6
+print("what number am I thinking of from 1 to 10?")
+user_input = input("number")
+int(user_imput)
+if user_imput = number
+    print("Thats right!")
+else:
+    print("wrong!")
+
+
+
+    
