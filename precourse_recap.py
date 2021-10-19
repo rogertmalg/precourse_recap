8a421123e53e4ecafe01944954388cc201278e56
rogertmalg <roger.malgueira@outlook.com>
2021-10-19 10:15:38 +0100

    final correction

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,7 +1,7 @@
 number = 6
 print("what number am I thinking of from 1 to 10?")
 user_input = input("enter number ")
-int(user_input)
+user_input = int(user_input)
 if user_input == number :
     print("Thats right!")
 else:
